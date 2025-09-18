1ee0110912c8c6baa2863e62556efcea59213a74
jprosvi <jprosvi@gmail.com>
2025-09-18 08:17:55 -0600

    Update version endpoint to v2

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ app = FastAPI(title = "Simple FastAPI website")
 def get_health():
     return {"msg": "ok"}
 
+@app.get('/readyz')
+def get_ready():
+    return {"msg": "Application ready"}
+
+
 @app.get('/')
 def root():
     return {"msg": "Hello World!"}
 
 
-
 @app.get('/version')
 def version():
 
-    return {"msg": "Version 1"}
+    return {"msg": "Version 2 with a new feature"}
